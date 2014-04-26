befc37c02a52818118314d9c03d8edba08615673
gunnxma <gunnxma@gmail.com>
2014-04-26 16:10:59 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fastnote::Application.config.secret_key_base = 'afb9ef3393c06c3a888fd6cbb7c8ccb56b4a2221b973dc89548d2aac7aa519430444fd9acddf6623b46d28ec5c437df0b123583a3166da6bf8867d48706f316f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fastnote::Application.config.session_store :cookie_store, key: '_fastnote_session'
