edd4aa42aea804d2027b3a17c68f60516d75b17a
马刚 <magang@magangtekiMac-mini-2.local>
2014-04-28 20:23:04 +0800

    list create destroy functionial finished

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,8 +1,30 @@
 class SectionsController < ApplicationController
   def index
+    @sections = Section.all.order('addtime DESC')
   end
 
   def new
     @section = Section.new
+  end
+
+  def create
+    @section = Section.new(section_params)
+    @section.addtime = Time.now
+
+    @section.save
+    redirect_to sections_path
+  end
+
+  def destroy
+    @section = Section.find(params[:id])
+    @section.destroy
+
+    redirect_to sections_path
+  end
+
+  private
+
+  def section_params
+    params.require(:section).permit(:content)
   end
 end
